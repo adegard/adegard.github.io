44a6cd01e336b7aa682573550a7a484a757fa620
adegard <degardinarnaud@gmail.com>
2022-05-15 19:14:19 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import react from "react"
-import reactDom from "react-dom"
-import App  from "./App"
-import {BrowserRouter} from "react-router-dom";
-
-
-const app = document.getElementById("app")
-
-reactDom.render( <BrowserRouter> <App /> </BrowserRouter>, app)
